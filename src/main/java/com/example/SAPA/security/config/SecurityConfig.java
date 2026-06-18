1c96b146a7ee1fcee1fd984914d05b0ab2afd8b9
Abby Derdoy <abrilgarile@gmail.com>
2026-06-18 01:15:23 -0300

    Frontend conectado con backend y registro de pacientes funcionando

--- src/main/java/com/example/SAPA/security/config/SecurityConfig.java
+++ src/main/java/com/example/SAPA/security/config/SecurityConfig.java
@@ -56,6 +56,18 @@ public class SecurityConfig {
 
                         .requestMatchers("/auth/**").permitAll()
                         .requestMatchers(HttpMethod.GET, "/specialities").permitAll()
+                        .requestMatchers(
+                                "/",
+                                "/login",
+                                "/registro-paciente",
+                                "/registro-medico"
+                        ).permitAll()
+
+                        .requestMatchers(
+                                "/css/**",
+                                "/img/**",
+                                "/js/**"
+                        ).permitAll()
 
                         .requestMatchers("/ws/**").permitAll()
                         .requestMatchers("/uploads/chat/**").permitAll()
