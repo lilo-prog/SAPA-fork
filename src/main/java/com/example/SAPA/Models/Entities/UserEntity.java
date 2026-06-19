79e556e4f8404a73de50fdde4c932ba6d4fe54c2
Brittany Marquez <britta.a.marquez@gmail.com>
2026-06-19 02:26:21 -0300

    Commit 2 19-06
    - Simplemente agregué un botón para volver al inicio en la pestaña mi perfil.

--- src/main/java/com/example/SAPA/Models/Entities/UserEntity.java
+++ src/main/java/com/example/SAPA/Models/Entities/UserEntity.java
@@ -17,7 +17,6 @@ public class UserEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
     private Long id;
 
     @Column(nullable = false, unique = true)
